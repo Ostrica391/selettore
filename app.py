e0bf2f88d14a222380e141c90776451827a8a5fe
Ostrica391 <alti94@hotmail.it>
2025-04-14 14:46:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ st.markdown("""
     <div class="top-menu">
         <a href="https://selettore-cs.streamlit.app/" target="_blank">Calcolatore CS</a>
         <a href="https://selettore-scl-adv.streamlit.app/" target="_blank">Calcolatore SCL-ADV</a>
-        <a href="https://www.tslac.it/">Work in progress</a>
+        <a href="https://selettoretslac.streamlit.app/">Calcolatore Sag Soft</a>
         <a href="https://www.tslac.it/">Work in progress</a>
     </div>
 """, unsafe_allow_html=True)
