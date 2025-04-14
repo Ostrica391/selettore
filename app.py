0cf33ca74f8a144172d25722ae8d6754ed8e55c0
Ostrica391 <alti94@hotmail.it>
2025-04-14 12:14:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -95,3 +95,8 @@ risultato_finale = risultato_base + valore_extra
 
 # Output
 st.subheader(f"Sag da ordinare: {risultato_finale:.2f}")
+
+# Calcolo del raggio base
+rb = ((val3 * 1000)**2 / (8000 * risultato)) + (risultato / 2000)
+
+st.markdown(f"<h4 style='color:#004890;'>Raggio base da ordinare: <b>{rb:.2f}</b> µm</h4>", unsafe_allow_html=True)
